1e048bdc1a7ed551e9fadfd8dd93bce66ea721c5
Pawel Niemczyk <p.niemczyk@pilot.co>
2015-05-26 20:58:05 +0200

    Refactoring

--- lib/light_mapper.rb
+++ lib/light_mapper.rb
@@ -6,23 +6,29 @@ module LightMapper
   end
 
   def self.mapping(hash, mappings, opts = {})
-    require_keys  = opts[:require_keys]  == true
-    any_keys_kind = opts[:any_keys_kind] == true
-    fetch_method  = if any_keys_kind
-                      if require_keys
-                        -> (h, k) { h.fetch(k.to_s, h.fetch(k.to_sym)) }
-                      else
-                        -> (h, k) { h[k.to_s] || h[k.to_sym] }
-                      end
-                    else
-                      if require_keys
-                        -> (h, k) { h.fetch(k) }
-                      else
-                        -> (h, k) { h[k] }
-                      end
-                    end
-    {}.tap do |h|
-      mappings.each { |k, v| h[v] = fetch_method.call(hash, k) }
+    method_name_parts = opts.each_with_object([]) { |(key, value), arr| arr << key.to_s if value == true  }.sort
+    method_name       = method_name_parts.empty? ? '_default_extraction' : "_#{method_name_parts.join('_and_')}_extraction"
+    fetch_method      = method(method_name)
+    mappings.each_with_object({}) do |(k, v), h|
+      h[v] = fetch_method.call(hash, k)
     end
+  end
+
+  private
+
+  def self._any_keys_kind_and_require_keys_extraction(hash, key)
+    hash.fetch(key.to_s, hash.fetch(key.to_sym))
+  end
+
+  def self._any_keys_kind_extraction(hash, key)
+    hash[key.to_s] || hash[key.to_sym]
+  end
+
+  def self._require_keys_extraction(hash, key)
+    hash.fetch(key)
+  end
+
+  def self._default_extraction(hash, key)
+    hash[key]
   end
 end
